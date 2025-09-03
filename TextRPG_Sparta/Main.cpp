38f12cf30422f26db885acc9beac32e0436c398c
Hyun <hnjogo@gmail.com>
2025-09-03 20:44:43 +0900

    DataManager
    - JsonParser : Json 파싱용 클래스

--- TextRPG_Sparta/Main.cpp
+++ TextRPG_Sparta/Main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main()
 {
+#ifdef _WIN32
+    // 콘솔 입출력 코드페이지를 UTF-8로
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     std::cout << "Hello World!\n";
 }
