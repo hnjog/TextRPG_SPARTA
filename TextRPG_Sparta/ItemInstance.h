f16e289b009f322a528bde436db5264a45c58186
Hyun <hnjogo@gmail.com>
2025-09-04 12:22:20 +0900

    ItemInstance Getter 추가

--- TextRPG_Sparta/ItemInstance.h
+++ TextRPG_Sparta/ItemInstance.h
@@ -6,10 +6,20 @@ class ItemInstance
 {
 public:
 	ItemInstance();
-	ItemInstance(int idx, std::string& name, std::string& effect, int value,int price, bool isConsumable, bool isStackable, int amount);
+	ItemInstance(int idx, std::string& name, std::string& effect, int value, int price, bool isConsumable, bool isStackable, int amount);
 	ItemInstance(ItemData& itemData, int amount);
 
 	bool UseItem(class CharacterBase* target);
+
+public:
+	inline int GetItemIdx() const { return itemData.idx; }
+	inline const std::string& GetItemName() const { return itemData.name; }
+	inline const std::string& GetItemEffect() const { return itemData.effect; }
+	inline int GetItemValue() const { return itemData.value; }
+	inline int GetItemPrice() const { return itemData.price; }
+	inline bool isConsumableItem() const { return itemData.isConsumable; }
+	inline bool isStackableItem() const { return itemData.isStackable; }
+	inline bool GetItemStock() const { return stock; }
 
 protected:
 	ItemData itemData;
