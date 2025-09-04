e65f1dffeb260ae522f57f379328e9cc03fdd18c
TheParkYB <park94128@naver.com>
2025-09-04 16:16:55 +0900

    상점 클래스가 있는 파일 추가

---
+++ TextRPG_Sparta/Shop.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Shop
+{
+private:
+	int shopID;//상점 번호
+	map<int, int> sellList;//판매 목록 <아이템 번호, 재고>
+
+public:
+	//상점 생성 (상점 번호 필요)
+	Shop(int shopID = 0);
+
+	//아이템 1종류를 지정한 개수만큼 추가
+	void AddItem(int itemID, int count = 1);
+
+	//아이템 여러 종류 각각 지정한 개수만큼 추가
+	//* itemIDList와 countList의 size는 동일해야 합니다.
+	void AddItemRange(vector<int> itemIDList, vector<int> countList);
+
+	//아이템 1종류를 지정한 개수만큼 제거
+	//제거 실패시 false
+	bool RemoveItem(int itemID, int count = 1);
+};
