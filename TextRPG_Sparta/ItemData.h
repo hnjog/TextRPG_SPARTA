caeaf7d3e705a3b28e6abbc129d6ca8aa61b9845
Hyun <hnjogo@gmail.com>
2025-09-04 11:57:31 +0900

    ItemData 생성자 추가

--- TextRPG_Sparta/ItemData.h
+++ TextRPG_Sparta/ItemData.h
@@ -11,4 +11,26 @@ struct ItemData
 	int price;
 	bool isConsumable;
 	bool isStackable;
+
+	ItemData()
+		:idx(0),
+		name(""),
+		effect(""),
+		value(0),
+		price(0),
+		isConsumable(false),
+		isStackable(false)
+	{
+	}
+
+	ItemData(int idx, std::string& name, std::string& effect, int value, bool isConsume, bool isStackable)
+		:idx(idx),
+		name(name),
+		effect(effect),
+		value(value),
+		price(price),
+		isConsumable(isConsumable),
+		isStackable(isStackable)
+	{
+	}
 };
