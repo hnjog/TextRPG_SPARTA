7c242c24514dce713758af2289d89410b1bd8fd7
Hyun <hnjogo@gmail.com>
2025-09-04 10:31:39 +0900

    Effect 용 struct,interface / regist / manager 파일 생성

---
+++ TextRPG_Sparta/EffectManager.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include"EffectDescriptor.h"
+#include<memory>
+#include<string>
+#include<unordered_map>
+
+class EffectManager
+{
+public:
+	EffectManager& GetInstance();
+	void Register(std::string& id, EffectFactory& ef);
+	std::unique_ptr<IEffect> Create(const std::string& id) const;
+
+private:
+	EffectManager();
+	std::unordered_map<std::string, EffectFactory> effectMap;
+};
+
---
+++ TextRPG_Sparta/EffectRegister.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include"EffectManager.h"
+
+namespace EffectTemplate
+{
+	template<class T>
+	void RegisterEffectType()
+	{
+		static_assert(std::is_base_of_v<IEffect, T>, "RegisterEffectType에 이상한 타입 집어넣지 마세요!");
+
+		EffectManager eManager = EffectManager::GetInstance();
+		// 내부에서 move로 가져가기에 
+		eManager.Register(T::GetId(), []() {return std::make_unique<T>(); })
+	}
+
+	template<class T>
+	struct EffectAutoRegister
+	{
+		EffectAutoRegister()
+		{
+			RegisterEffectType<T>();
+		}
+	};
+
+#define REGISTER_EFFECT_TYPE(T) static EffectAutoRegister<T> _auto_register_##T;
+}
