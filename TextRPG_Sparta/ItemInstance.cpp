ade71c7309f1461712966438f356c80559bb64a6
Hyun <hnjogo@gmail.com>
2025-09-04 11:58:11 +0900

    ItemInstance 추가
    - 차후 CharacterBase 추가 예정(include)

---
+++ TextRPG_Sparta/ItemInstance.cpp
@@ -0,0 +1,48 @@
+﻿#include "ItemInstance.h"
+#include "EffectManager.h"
+#include "EffectDescriptor.h"
+
+ItemInstance::ItemInstance()
+	:stock(0)
+{
+}
+
+ItemInstance::ItemInstance(int idx, std::string& name, std::string& effect, int value, int price, bool isConsumable, bool isStackable, int amount)
+{
+	itemData.idx = idx;
+	itemData.name = name;
+	itemData.effect = effect;
+	itemData.value = value;
+	itemData.price = price;
+	itemData.isConsumable = isConsumable;
+	itemData.isStackable = isStackable;
+
+	if (itemData.isStackable)
+		stock = amount;
+	else
+		stock = 1;
+}
+
+ItemInstance::ItemInstance(ItemData& itemData, int amount)
+	:ItemInstance(itemData.idx,itemData.name,itemData.effect,itemData.value,itemData.price,itemData.isConsumable,itemData.isStackable,amount)
+{
+}
+
+bool ItemInstance::UseItem(CharacterBase* target)
+{
+	EffectManager& effectManager = EffectManager::GetInstance();
+	EffectContext context;
+	context.target = target;
+	context.value = itemData.value;
+	if (effectManager.Create(itemData.effect)->Apply(context))
+	{
+		if (itemData.isConsumable)
+		{
+			stock--;
+		}
+
+		return true;
+	}
+
+	return false;
+}
---
+++ TextRPG_Sparta/ItemInstance.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include"ItemData.h"
+
+class ItemInstance
+{
+public:
+	ItemInstance();
+	ItemInstance(int idx, std::string& name, std::string& effect, int value,int price, bool isConsumable, bool isStackable, int amount);
+	ItemInstance(ItemData& itemData, int amount);
+
+	bool UseItem(class CharacterBase* target);
+
+protected:
+	ItemData itemData;
+	int stock;
+};
+
