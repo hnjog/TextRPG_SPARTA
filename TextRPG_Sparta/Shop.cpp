09b6eb98135793a4bddab69297da54c9d4090738
TheParkYB <park94128@naver.com>
2025-09-05 20:18:19 +0900

    아이템 판매 함수 구현 (진행중)

--- TextRPG_Sparta/Shop.cpp
+++ TextRPG_Sparta/Shop.cpp
@@ -109,3 +109,31 @@ ShopMessage Shop::CheckPrice(_Out_ int* price, int itemID, int count)
 	*price = itemData->price * count;
 	return ShopMessage::OK;
 }
+
+ShopMessage Shop::SellItem(_Out_ ItemData* sellItem, int itemID, int count = 1)
+{
+	sellItem = nullptr;
+
+	//올바른 아이템 번호가 아님
+	const ItemData* itemData = ItemManager::GetInstance().GetItemData(itemID);
+	if (itemData == nullptr)
+		return ShopMessage::INCORRECT_ITEM;
+
+	//판매중인 아이템이 아님
+	if (sellList.find(itemID) == sellList.end())
+		return ShopMessage::NO_SELLING_ITEM;
+
+	//구매하려는 개수가 0 이하
+	if (count <= 0)
+		return ShopMessage::INCORRECT_INPUT;
+
+	//구매하려는 개수가 재고보다 많음
+	if (sellList[itemID] < count)
+		return ShopMessage::NOT_ENOUGH_STOCK;
+
+	//판매 처리
+	sellList[itemID] -= count;
+	sellItem = nullptr;//TODO : 올바른 아이템 데이터를 반환해야함
+
+	return ShopMessage::OK;
+}
