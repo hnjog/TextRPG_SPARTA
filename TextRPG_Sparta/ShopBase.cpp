f6f51c9e2a463600fa1bf715f663601de2efed3e
TheParkYB <park94128@naver.com>
2025-09-08 20:50:43 +0900

    상점 메세지를 사용하는 기능을 폐지하고 적절한 반환형으로 지정

--- TextRPG_Sparta/ShopBase.cpp
+++ TextRPG_Sparta/ShopBase.cpp
@@ -9,10 +9,13 @@ ShopBase::ShopBase(int shopID, string shopName)
 	name = shopName;
 }
 
-ShopMessage ShopBase::PrintSellingItems()
+void ShopBase::PrintSellingItems()
 {
 	if (sellList.empty())
-		return ShopMessage::OTHER_ERROR;
+	{
+		cout << "ERROR : 판매중인 아이템이 없습니다." << endl;
+		return;
+	}
 
 	cout << "========================================" << endl;
 	cout << "* 판매 아이템 목록" << endl;
@@ -20,36 +23,37 @@ ShopMessage ShopBase::PrintSellingItems()
 		cout << sellInfo.first << " : " << sellInfo.second << endl;
 	
 	cout << "========================================" << endl;
-
-	return ShopMessage::OK;
 }
 
-ShopMessage ShopBase::AddItem(int itemID, int count)
+bool ShopBase::AddItem(int itemID, int count)
 {
 	if (count <= 0)//1개 이상 추가 필요
-		return ShopMessage::INCORRECT_INPUT;
+	{
+		cout << "ERROR : 추가할 개수가 0 이하입니다." << endl;
+		return false;
+	}
 
 	sellList[itemID] += count;
 
-	return ShopMessage::OK;
+	return true;
 }
 
-ShopMessage ShopBase::AddItemRange(vector<int> itemIDList, vector<int> countList)
+bool ShopBase::AddItemRange(vector<int> itemIDList, vector<int> countList)
 {
 	int length = itemIDList.size();
 
 	//두 vector의 길이가 서로 불일치
 	if (length == 0 || length != countList.size())
-		return ShopMessage::INCORRECT_INPUT;
+	{
+		cout << "ERROR : itemIDList와 countList의 size가 0이거나 불일치합니다." << endl;
+		return false;
+	}
 
 	//countList중 0이하로 입력된 값이 있음
 	if (any_of(countList.begin(), countList.end(), [](int count) { return count <= 0; }))
-		return ShopMessage::INCORRECT_INPUT;
-
-	for (int& count : countList)
 	{
-		if (count <= 0)
-			return ShopMessage::INCORRECT_INPUT;
+		cout << "ERROR : 추가할 개수가 0 이하인 값이 있습니다." << endl;
+		return false;
 	}
 
 	//지정한 아이템을 해당 개수만큼 추가
@@ -58,76 +62,107 @@ ShopMessage ShopBase::AddItemRange(vector<int> itemIDList, vector<int> countList
 		sellList[itemIDList[iNum]] += countList[iNum];
 	}
 
-	return ShopMessage::OK;
+	return true;
 }
 
-ShopMessage ShopBase::RemoveItem(int itemID, int count)
+bool ShopBase::RemoveItem(int itemID, int count)
 {
 	//판매중인 아이템이 아님
 	if (sellList.find(itemID) == sellList.end())
-		return ShopMessage::NO_SELLING_ITEM;
+	{
+		cout << "ERROR : 판매중인 아이템이 아닙니다." << endl;
+		return false;
+	}
 
 	//1개 이상 제거 필요
 	if (count <= 0)
-		return ShopMessage::INCORRECT_INPUT;
+	{
+		cout << "ERROR : 제거할 개수가 0 이하입니다." << endl;
+		return false;
+	}
 
 	//개수 부족
 	if (sellList[itemID] < count)
-		return ShopMessage::NOT_ENOUGH_STOCK;
+	{
+		cout << "ERROR : 제거할 개수가 판매중인 개수보다 많습니다." << endl;
+		return false;
+	}
 
 	//제거 성공
 	sellList[itemID] -= count;
-	return ShopMessage::OK;
+	return false;
 }
 
-ShopMessage ShopBase::CheckPrice(int& price, int itemID, int count)
-{
-	price = 0;
-
-	//올바른 아이템 번호가 아님
-	const ItemData* itemData = ItemManager::GetInstance().GetItemData(itemID);
-	if (itemData == nullptr)
-		return ShopMessage::INCORRECT_ITEM;
-
-	//판매중인 아이템이 아님
-	if (sellList.find(itemID) == sellList.end())
-		return ShopMessage::NO_SELLING_ITEM;
-
-	//구매하려는 개수가 0 이하
-	if (count <= 0)
-		return ShopMessage::INCORRECT_INPUT;
-
-	//구매하려는 개수가 재고보다 많음
-	if (sellList[itemID] < count)
-		return ShopMessage::NOT_ENOUGH_STOCK;
-
-	//가격 계산
-	price = itemData->price * count;
-	return ShopMessage::OK;
-}
-
-ShopMessage ShopBase::SellItem(ItemData& sellItem, int itemID, int count)
+int ShopBase::CheckPrice(int itemID, int count)
 {
 	//올바른 아이템 번호가 아님
 	const ItemData* itemData = ItemManager::GetInstance().GetItemData(itemID);
 	if (itemData == nullptr)
-		return ShopMessage::INCORRECT_ITEM;
+	{
+		cout << "ERROR : 아이템 번호를 확인해주세요." << endl;
+		return 0;
+	}
 
 	//판매중인 아이템이 아님
 	if (sellList.find(itemID) == sellList.end())
-		return ShopMessage::NO_SELLING_ITEM;
+	{
+		cout << "ERROR : 판매중인 아이템이 아닙니다." << endl;
+		return 0;
+	}
 
 	//구매하려는 개수가 0 이하
 	if (count <= 0)
-		return ShopMessage::INCORRECT_INPUT;
+	{
+		cout << "ERROR : 구매할 개수가 0 이하입니다." << endl;
+		return 0;
+	}
 
 	//구매하려는 개수가 재고보다 많음
 	if (sellList[itemID] < count)
-		return ShopMessage::NOT_ENOUGH_STOCK;
+	{
+		cout << "ERROR : 구매할 개수가 판매중인 개수보다 많습니다." << endl;
+		return 0;
+	}
+
+	//가격 계산하고 반환
+	return itemData->price * count;
+}
+
+ItemData* ShopBase::SellItem(int itemID, int count)
+{
+	//올바른 아이템 번호가 아님
+	const ItemData* itemData = ItemManager::GetInstance().GetItemData(itemID);
+	if (itemData == nullptr)
+	{
+		cout << "ERROR : 아이템 번호를 확인해주세요." << endl;
+		return nullptr;
+	}
+
+	//판매중인 아이템이 아님
+	if (sellList.find(itemID) == sellList.end())
+	{
+		cout << "ERROR : 판매중인 아이템이 아닙니다." << endl;
+		return nullptr;
+	}
+
+	//구매하려는 개수가 0 이하
+	if (count <= 0)
+	{
+		cout << "ERROR : 구매할 개수가 0 이하입니다." << endl;
+		return nullptr;
+	}
+
+	//구매하려는 개수가 재고보다 많음
+	if (sellList[itemID] < count)
+	{
+		cout << "ERROR : 구매할 개수가 판매중인 개수보다 많습니다." << endl;
+		return nullptr;
+	}
 
 	//판매 처리
+	//TODO : 올바른 데이터를 반환하도록 변경
 	sellList[itemID] -= count;
-	sellItem = *itemData;
-
-	return ShopMessage::OK;
+	//ItemData* sellItemData(itemData);
+	//return sellItemData;
+	return nullptr;
 }
--- TextRPG_Sparta/ShopBase.h
+++ TextRPG_Sparta/ShopBase.h
@@ -8,15 +8,15 @@ using namespace std;
 struct ItemData;
 
 //상점 기능 이용시 처리 결과
-enum class ShopMessage
-{
-	OK,//문제 없음
-	INCORRECT_ITEM,//아이템 ID가 올바르지 않음
-	INCORRECT_INPUT,//아이템 ID외 입력이 올바르지 않음
-	NO_SELLING_ITEM,//판매중인 아이템이 아님
-	NOT_ENOUGH_STOCK,//재고 부족
-	OTHER_ERROR,//기타 다른 오류
-};
+//enum class ShopMessage
+//{
+//	OK,//문제 없음
+//	INCORRECT_ITEM,//아이템 ID가 올바르지 않음
+//	INCORRECT_INPUT,//아이템 ID외 입력이 올바르지 않음
+//	NO_SELLING_ITEM,//판매중인 아이템이 아님
+//	NOT_ENOUGH_STOCK,//재고 부족
+//	OTHER_ERROR,//기타 다른 오류
+//};
 
 class ShopBase
 {
@@ -30,37 +30,42 @@ public:
 	ShopBase(int shopID = 0, string shopName = "");
 
 	//판매중인 아이템을 출력한다.
-	//* OTHER_ERROR : 판매중인 아이템이 없음
-	ShopMessage PrintSellingItems();
+	void PrintSellingItems();
 
 	//아이템 1종류를 지정한 개수만큼 추가
-	//* INCORRECT_INPUT : 추가할 개수가 0 이하
-	ShopMessage AddItem(int itemID, int count = 1);
+	//=== false 사유 ===
+	//* 추가할 개수가 0 이하
+	bool AddItem(int itemID, int count = 1);
 
 	//아이템 여러 종류 각각 지정한 개수만큼 추가
-	//* INCORRECT_INPUT : itemIDList와 countList의 size가 0이거나 불일치 또는 추가할 개수가 0 이하인 값이 있음
-	ShopMessage AddItemRange(vector<int> itemIDList, vector<int> countList);
+	//=== false 사유 ===
+	//* itemIDList와 countList의 size가 0이거나 불일치 
+	//* 추가할 개수가 0 이하인 값이 있음
+	bool AddItemRange(vector<int> itemIDList, vector<int> countList);
 
 	//아이템 1종류를 지정한 개수만큼 제거
-	//* NO_SELLING_ITEM : 판매중인 아이템이 아님
-	//* INCORRECT_INPUT : 제거할 개수가 0 이하
-	//* NOT_ENOUGH_STOCK : 제거할 개수가 재고보다 많음
-	ShopMessage RemoveItem(int itemID, int count = 1);
+	//=== false 사유 ===
+	//* 판매중인 아이템이 아님
+	//* 제거할 개수가 0 이하
+	//* 제거할 개수가 재고보다 많음
+	bool RemoveItem(int itemID, int count = 1);
 
 	//지정한 아이템과 개수가 얼마인지 확인
-	//성공적으로 처리시 price변수에 값이 저장된다. 에러 발생시 0이 저장된다.
-	//* INCORRECT_ITEM : 올바른 아이템 번호가 아님
-	//* NO_SELLING_ITEM : 판매중인 아이템이 아님
-	//* INCORRECT_INPUT : 구매하려는 개수가 0 이하
-	//* NOT_ENOUGH_STOCK : 구매하려는 개수가 재고보다 많음
-	ShopMessage CheckPrice(int& price, int itemID, int count = 1);
+	//성공적으로 처리시 그 값이 반환화된다. 에러 발생시 0이 반환된다.
+	//=== 0 반환 사유 ===
+	//* 올바른 아이템 번호가 아님
+	//* 판매중인 아이템이 아님
+	//* 구매하려는 개수가 0 이하
+	//* 구매하려는 개수가 재고보다 많음
+	int CheckPrice(int itemID, int count = 1);
 
-	//아이템 판매
+	//아이템 판매 (구현중)
 	//상점 입장에서 판매이므로 캐릭터 입장에서는 구매
-	//성공적으로 처리시 판매할 아이템이 sellItem에 담기며 실패시 변화 없음
-	//* INCORRECT_ITEM : 올바른 아이템 번호가 아님
-	//* NO_SELLING_ITEM : 판매중인 아이템이 아님
-	//* INCORRECT_INPUT : 구매하려는 개수가 0 이하
-	//* NOT_ENOUGH_STOCK : 구매하려는 개수가 재고보다 많음
-	ShopMessage SellItem(ItemData& sellItem, int itemID, int count = 1);
+	//성공적으로 처리시 판매할 아이템이 sellItem에 담기며 실패시 null 반환
+	//=== null 반환 사유 ===
+	//* 올바른 아이템 번호가 아님
+	//* 판매중인 아이템이 아님
+	//* 구매하려는 개수가 0 이하
+	//* 구매하려는 개수가 재고보다 많음
+	ItemData* SellItem(int itemID, int count = 1);
 };
