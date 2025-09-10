bcf1a7f1eb3bddb00e0d3093d34dd34284d98a13
ko gyeong tae <rudex1598@gmail.com>
2025-09-10 15:59:10 +0900

    오타 수정 공격력 회북 -> 공격력 증가

--- TextRPG_Sparta/Character/Player.cpp
+++ TextRPG_Sparta/Character/Player.cpp
@@ -95,7 +95,7 @@ void Player::AddExp(int exp)
 			cout << GetName() << "의 레벨이 상승하였습니다." << endl << "현재 레벨: " << m_level << endl;
 			cout << "최대 체력 증가, hp가 회복되었습니다. " << endl
 				<< "최대 체력: " << GetMaxHp() << endl;
-			cout << "공격력이 회복되었습니다. " << endl
+			cout << "공격력이 증가되었습니다. " << endl
 				<< "공격력: " << GetAttack() << endl;
 		}
 	}
